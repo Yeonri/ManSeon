c817858b9eb39b614d5bcaf83ccc52396af230f2
최이화 <ihwa220@naver.com>
2025-03-19 00:06:55 +0900

    feat: svg 파일 추가

--- FE/frontend/App.tsx
+++ FE/frontend/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import "./global.css";
+import Home from "./src/assets/images/icon_home.svg";
 
 export default function App(): React.JSX.Element {
   return (
     <View>
       <Text className="bg-blue-500">만선</Text>
+      <Home />
     </View>
   );
 }
--- FE/frontend/metro.config.js
+++ FE/frontend/metro.config.js
@@ -1,11 +1,16 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import("@react-native/metro-config").MetroConfig}
- */
-const config = mergeConfig(getDefaultConfig(__dirname), {});
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = mergeConfig(defaultConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
+  },
+});
 
 module.exports = withNativeWind(config, { input: "./global.css" });
