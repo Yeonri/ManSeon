c5534254b72e8922abf24dca10f89e220c852c03
최이화 <ihwa220@naver.com>
2025-03-18 22:38:29 +0900

    feat: NativeWind 추가

--- FE/frontend/App.tsx
+++ FE/frontend/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import "./global.css";
 
 export default function App(): React.JSX.Element {
   return (
     <View>
-      <Text>만선</Text>
+      <Text className="bg-blue-500">만선</Text>
     </View>
   );
 }
