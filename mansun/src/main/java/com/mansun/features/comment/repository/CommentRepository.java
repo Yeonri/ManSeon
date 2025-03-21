74c87cc40928c2059d5216817a155da7e107c96d
이진형 <ddocdoli@naver.com>
2025-03-21 21:34:04 +0900

    feat : 댓글 기능 구현 준비

---
+++ mansun/src/main/java/com/mansun/features/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.mansun.features.comment.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+}
---
+++ mansun/src/main/java/com/mansun/features/comment/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.mansun.features.comment.service;
+
+import com.mansun.features.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public interface CommentService {
+
+}
