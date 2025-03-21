407dbb3a9dfdb31c0f75fd3c299be02a5036c64e
이진형 <ddocdoli@naver.com>
2025-03-21 14:37:25 +0900

    feat : 회원가입과 JWT 토큰을 이용한 로그인 기능 추가

---
+++ mansun/src/main/java/com/mansun/common/auth/jwt/JwtFilter.java
@@ -0,0 +1,57 @@
+package com.mansun.common.auth.jwt;
+
+import com.mansun.common.auth.CustomUserDetails;
+import com.mansun.entity.Users;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+        //토큰이 없을 경우
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        //토큰이 만료되었을 경우
+        if (jwtUtil.isExpired(token)) {
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = jwtUtil.getUsername(token);
+        String userId = jwtUtil.getuserId(token);
+
+        Users user = new Users();
+        user.setEmail(email);
+        user.setPassword("");
+        user.setUserId(Long.parseLong(userId));
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);//UserSession 생성
+
+        filterChain.doFilter(request, response);
+    }
+}
--- mansun/src/main/java/com/mansun/requestDto/user/loginUserReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.mansun.requestDto.user;
-
-import lombok.Getter;
-
-@Getter
-public class loginUserReqDto {
-    String email;
-    String password;
-}
