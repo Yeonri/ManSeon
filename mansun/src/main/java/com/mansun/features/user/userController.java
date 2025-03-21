c54e5c4d198ec3e07955720f0ea87cb3052859b0
이진형 <ddocdoli@naver.com>
2025-03-21 15:30:02 +0900

    feat : 유저 회원가입, 로그인, 삭제 기능 추가
    회원 정보 수정 기능 미구현

--- mansun/src/main/java/com/mansun/features/user/userController.java
+++ mansun/src/main/java/com/mansun/features/user/userController.java
@@ -1,7 +1,65 @@
 package com.mansun.features.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mansun.common.auth.CustomUserDetails;
+import com.mansun.entity.Users;
+import com.mansun.features.user.service.UserServiceImpl;
+import com.mansun.requestDto.user.createUserReqDto;
+import com.mansun.requestDto.user.updateUserReqDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-public class userController {
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+@Tag(name = "UserController",description = "사용자 정보의 CRUD를 담당하는 컨트롤러")
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            schema = @Schema(type = "string",example = "회원가입 성공")
+                    )),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody createUserReqDto req) {
+        //아무것도 추가하지 않고 헤더만 추가해서 간다.
+        //만약 오류가 난다면 service 안에서 예외처리 반환 예정
+        userService.createUser(req);
+        return ResponseEntity.ok("회원가입 성공");
+    }
+
+    // 로그인 과정의 대략적인 설명 : 해당 요청 mapping은 이곳에 있지 않다.
+    // service의 loadbyusername 참고
+    // Service에서는 우선 입력받은 email과 password를 이용해서 DB에서 사람을 찾는다.
+    // 해당 사람을 찾을 경우는 CustomUserDetails란 이름으로 객체를 생성해서 그 안에 해당 유저를 넣으면 Security Logic 작동
+
+    @PatchMapping
+    public ResponseEntity<String> updateUser(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestBody updateUserReqDto req){
+        Users user=userService.updateUser(customUserDetails.getUserId(),customUserDetails.getUsername());
+        return ResponseEntity.ok("회원 정보가 성공적으로 변경되었습니다.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteUser(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        System.out.println(customUserDetails.getUserId());
+        userService.deleteUser(customUserDetails.getUserId());
+        return ResponseEntity.ok("회원 정보가 삭제되었습니다.");
+    }
 }
