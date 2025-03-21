1ea60cc4bcedef24a88a7b4b332862e606e0ff21
이진형 <ddocdoli@naver.com>
2025-03-21 21:41:23 +0900

    feat : 댓글 기능 구현 준비(2)

--- mansun/src/main/java/com/mansun/features/comment/commentController.java
+++ mansun/src/main/java/com/mansun/features/comment/commentController.java
@@ -1,6 +1,7 @@
 package com.mansun.features.comment;
 
 import com.mansun.features.comment.service.CommentService;
+import com.mansun.features.comment.service.CommentServiceImpl;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "CommentController",description = "댓글의 CRUD를 담당하는 컨트롤러")
 public class CommentController {
-    private final CommentService commentService;
+    private final CommentServiceImpl commentService;
 
 
 }
