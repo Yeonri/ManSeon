ff8b5ce14dd376c60821919de37c37fcf5283fbe
최이화 <ihwa220@naver.com>
2025-03-21 14:41:42 +0900

    fix: LF로 변경

--- FE/frontend/src/screens/mainScreen/index.tsx
+++ FE/frontend/src/screens/mainScreen/index.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { Image, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function MainScreen() {
   return (
-    <View>
-      <Text>메인</Text>
-    </View>
+    // Nativewind 확인용 코드 (추후 삭제 예정)
+    <SafeAreaView>
+      <Text className="bg-blue-500">메인</Text>
+      <Image
+        source={{
+          uri: "https://i.pinimg.com/736x/88/75/dc/8875dcbcd3ffe823e095f06cc03f679d.jpg",
+        }}
+        className="w-96 h-96"
+      />
+    </SafeAreaView>
   );
 }
