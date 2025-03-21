ef0736fd52acfa3b032100634217a08da0602d50
이진형 <ddocdoli@naver.com>
2025-03-21 15:29:25 +0900

    fix : 회원가입 요청,응답 DTO 수정

--- mansun/src/main/java/com/mansun/requestDto/user/createUserReqDto.java
+++ mansun/src/main/java/com/mansun/requestDto/user/createUserReqDto.java
@@ -1,12 +1,19 @@
 package com.mansun.requestDto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(name = "회원가입할 유저의 필요 정보")
 public class createUserReqDto {
+    @Schema(defaultValue = "1234@naver.com")
     private String email;
+    @Schema(defaultValue = "1234")
     private String password;
+    @Schema(defaultValue = "1234")
     private String name;
+    @Schema(defaultValue = "01012341234")
     private String phoneNum;
+    @Schema(defaultValue = "1234")
     private String nickname;
 }
--- mansun/src/main/java/com/mansun/responseDto/user/createUserResDto.java
+++ mansun/src/main/java/com/mansun/responseDto/user/createUserResDto.java
@@ -1,4 +1,9 @@
 package com.mansun.responseDto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Schema
+@Getter
 public class createUserResDto {
 }
