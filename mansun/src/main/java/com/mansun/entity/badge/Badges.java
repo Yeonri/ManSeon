55b11fb15f6fe98c4f76f087d06925117272d43a
이진형 <ddocdoli@naver.com>
2025-03-23 22:04:37 +0900

    feat : Badge 기능 추가
    branch 전환 깜빡해서 comment branch에서 변경

--- mansun/src/main/java/com/mansun/entity/badge/Badges.java
+++ mansun/src/main/java/com/mansun/entity/badge/Badges.java
@@ -1,19 +1,30 @@
 package com.mansun.entity.badge;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.annotation.processing.Generated;
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(title = "서비스에서 제공하는 뱃지의 종류",description = "서비스 전체 뱃지의 이름과 이미지를 가지고 있는 Dto")
 public class Badges {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "뱃지 아이디", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long badgeId;
 
 //    Column
+    @Schema(description = "뱃지 이름")
     private String badgeName;
+    @Schema(description = "뱃지 이미지 경로")
     private String badgeImg;
 }
--- mansun/src/main/java/com/mansun/entity/badge/UserBadge.java
+++ mansun/src/main/java/com/mansun/entity/badge/UserBadge.java
@@ -2,20 +2,28 @@ package com.mansun.entity.badge;
 
 import com.mansun.entity.Users;
 import com.mansun.entity.badge.Badges;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(title = "유저가 가지고 있는 뱃지 종류", description = "회원이 가진 뱃지와 전체 뱃지를 확인하는 중간 Entity")
 public class UserBadge {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "primaryKey")
     private Long pk;
 //    연관 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
+    @Schema(title="회원")
     private Users user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
+    @Schema(title="회원이 소유한 뱃지")
     private Badges badge;
 }
