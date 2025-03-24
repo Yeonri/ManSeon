f613eedc3f41b6871526466170b29f30fc35bd60
이진형 <ddocdoli@naver.com>
2025-03-24 16:18:14 +0900

    feat : 대댓글 기능 수정

--- mansun/src/main/java/com/mansun/features/recomment/service/RecommentServiceImpl.java
+++ mansun/src/main/java/com/mansun/features/recomment/service/RecommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mansun.features.recomment.service;
 
+import com.mansun.features.recomment.repository.RecommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class RecommentServiceImpl implements RecommentService{
-    private final RecommentServiceImpl repository;
+    private final RecommentRepository repository;
 
+    public void createRecomment() {
+
+    }
 }
---
+++ mansun/src/main/java/com/mansun/requestDto/recomment/createRecommentReqDto.java
@@ -0,0 +1,9 @@
+package com.mansun.requestDto.recomment;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class createRecommentReqDto {
+}
