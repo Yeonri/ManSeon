aa56b568b5d2cce43f399b45d3972e5eb22b8fd1
이진형 <ddocdoli@naver.com>
2025-03-24 15:31:42 +0900

    feat : recomment 기능 CUD 기능 추가

---
+++ mansun/src/main/java/com/mansun/features/recomment/RecommentController.java
@@ -0,0 +1,11 @@
+package com.mansun.features.recomment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recomment")
+@RequiredArgsConstructor
+public class RecommentController {
+}
