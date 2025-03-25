91490750770d32f4677b7858b16c8801096d35ab
최이화 <ihwa220@naver.com>
2025-03-26 00:19:40 +0900

    feat: 댓글, 답글 삭제 추가

--- FE/frontend/src/components/community/commentList/index.tsx
+++ FE/frontend/src/components/community/commentList/index.tsx
@@ -1,15 +1,32 @@
 import { formatDistanceToNow } from "date-fns";
 import { ko } from "date-fns/locale";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Comment } from "../../../api/types/Comment";
 import TagFollow from "../../../assets/images/tag_follow.svg";
 import { AddRecomment } from "../addRecomment";
 import { RecommentList } from "../recommentList";
+import IconEdit from "../../../assets/images/icon_edit.svg";
+import IconDelete from "../../../assets/images/icon_delete.svg";
 
 export function CommentList({ comments }: { comments: Comment[] }) {
   function formatTime(time: string) {
     return formatDistanceToNow(new Date(time), { addSuffix: true, locale: ko });
   }
+
+  function createDeleteAlert() {
+    Alert.alert("댓글 삭제", "댓글을 삭제하시겠습니까?", [
+      { text: "아니오", onPress: () => {} },
+      { text: "네", onPress: () => {} },
+    ]);
+  }
+
   return (
     <FlatList
       data={comments}
@@ -29,12 +46,23 @@ export function CommentList({ comments }: { comments: Comment[] }) {
               </TouchableOpacity>
               <TouchableOpacity onPress={() => {}} className="h-4">
                 <TagFollow />
+                {/* <TagFollowing /> */}
               </TouchableOpacity>
-              {/* <TagFollowing /> */}
             </View>
-            <Text className="text-neutral-400 text-sm">
-              {formatTime(item.createAt)}
-            </Text>
+            <View className="flex-row items-center gap-1">
+              <TouchableOpacity onPress={() => {}} className="h-6">
+                <IconEdit />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => createDeleteAlert()}
+                className="h-6"
+              >
+                <IconDelete />
+              </TouchableOpacity>
+              <Text className="text-neutral-400 text-sm">
+                {formatTime(item.createAt)}
+              </Text>
+            </View>
           </View>
 
           <Text className="ml-10">{item.commentContent}</Text>
--- FE/frontend/src/components/community/recommentList/index.tsx
+++ FE/frontend/src/components/community/recommentList/index.tsx
@@ -1,13 +1,29 @@
 import { formatDistanceToNow } from "date-fns";
 import { ko } from "date-fns/locale";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Recomment } from "../../../api/types/Recomment";
 import IconArrow from "../../../assets/images/icon_arrow.svg";
 import TagFollow from "../../../assets/images/tag_follow.svg";
+import IconEdit from "../../../assets/images/icon_edit.svg";
+import IconDelete from "../../../assets/images/icon_delete.svg";
 
 export function RecommentList({ recomments }: { recomments: Recomment[] }) {
   function formatTime(time: string) {
     return formatDistanceToNow(new Date(time), { addSuffix: true, locale: ko });
+  }
+
+  function createDeleteAlert() {
+    Alert.alert("답글 삭제", "답글을 삭제하시겠습니까?", [
+      { text: "아니오", onPress: () => {} },
+      { text: "네", onPress: () => {} },
+    ]);
   }
 
   return (
@@ -32,9 +48,20 @@ export function RecommentList({ recomments }: { recomments: Recomment[] }) {
                   </TouchableOpacity>
                 </View>
               </View>
-              <Text className="text-neutral-400 text-sm">
-                {formatTime(item.createAt)}
-              </Text>
+              <View className="flex-row items-center gap-1">
+                <TouchableOpacity onPress={() => {}} className="h-6">
+                  <IconEdit />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => createDeleteAlert()}
+                  className="h-6"
+                >
+                  <IconDelete />
+                </TouchableOpacity>
+                <Text className="text-neutral-400 text-sm">
+                  {formatTime(item.createAt)}
+                </Text>
+              </View>
             </View>
             <Text className="ml-16">{item.recommentContent}</Text>
           </View>
