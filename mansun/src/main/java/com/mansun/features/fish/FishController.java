e548c5c2499abfa32e4db96ef10f60745cae0417
이진형 <ddocdoli@jbnu.ac.kr>
2025-03-25 23:23:14 +0900

    fix : Fish Controller reqDto 이름 변경

--- mansun/src/main/java/com/mansun/features/fish/FishController.java
+++ mansun/src/main/java/com/mansun/features/fish/FishController.java
@@ -1,9 +1,16 @@
 package com.mansun.features.fish;
 
+import com.mansun.common.auth.CustomUserDetails;
 import com.mansun.features.fish.service.FishTypeServiceImpl;
+
 import com.mansun.requestDto.fish.CreateFishReqDto;
+import com.mansun.requestDto.fish.CreateFishTypeReqDto;
+import com.mansun.responseDto.OnlyMessageResDto;
+
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/fish")
 @RequiredArgsConstructor
 public class FishController {
-    private final FishTypeServiceImpl fishTypeService;
+	private final FishTypeServiceImpl fishTypeService;
 
-    @PostMapping
-    public ResponseEntity<String> addNewFish(CreateFishReqDto req){
+	// 어종은 추가될 뿐 수정되거나 삭제되지 않는다. 
+	@Operation(summary = "전체 어종의 추가 기능")
+	@PostMapping
+	public ResponseEntity<OnlyMessageResDto> addNewFishType(CreateFishTypeReqDto req) {
 
-        return ResponseEntity.ok("성공적으로 어종이 추가되었습니다.");
-    }
-
+		return ResponseEntity.ok(new OnlyMessageResDto("성공적으로 어종이 추가되었습니다."));
+	}
 }
--- mansun/src/main/java/com/mansun/requestDto/fish/CreateFishTypeReqDto.java
+++ mansun/src/main/java/com/mansun/requestDto/fish/CreateFishTypeReqDto.java
@@ -5,5 +5,5 @@ import lombok.Getter;
 
 @Builder
 @Getter
-public class CreateFishReqDto {
+public class CreateFishTypeReqDto {
 }
