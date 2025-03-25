971a48cc6a5242ab31498db0270e4b4a68805fed
이진형 <ddocdoli@naver.com>
2025-03-25 13:42:40 +0900

    fix : Board 쿼리 메소드 수정

--- mansun/src/main/java/com/mansun/features/board/repository/BoardRepository.java
+++ mansun/src/main/java/com/mansun/features/board/repository/BoardRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
 //    Board에서 userId에 해당하는 모든 게시물을 가져온다 페이징 미적용
-    public List<Board> findBoardsByUser_UserId(Long userId);
 
 //  _ 사용은 https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html의 property Expression 참고
     public List<Board> findBoardsByUser_UserIdAndPostId(Long userId, Long postId);
 
+    public List<Board> findByUser_UserId(Long userId);
 }
