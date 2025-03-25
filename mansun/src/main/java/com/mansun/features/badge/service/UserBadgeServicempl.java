5c81d72d7162c2770180e6f5fe93e9ca057192f4
이진형 <ddocdoli@naver.com>
2025-03-25 13:42:56 +0900

    fix : userBadge 쿼리 메소드 수정

--- mansun/src/main/java/com/mansun/features/badge/service/UserBadgeServicempl.java
+++ mansun/src/main/java/com/mansun/features/badge/service/UserBadgeServicempl.java
@@ -2,6 +2,7 @@ package com.mansun.features.badge.service;
 
 import com.mansun.common.auth.CustomUserDetails;
 import com.mansun.entity.badge.Badges;
+import com.mansun.entity.badge.UserBadge;
 import com.mansun.features.badge.repository.UserBadgeRepository;
 import com.mansun.responseDto.badges.allBadgesByUserResDto;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,13 @@ public class UserBadgeServicempl implements UserBadgeService{
     private final UserBadgeRepository userBadgeRepository;
 
     public List<allBadgesByUserResDto> getAllBadgesByUser(CustomUserDetails customUserDetails){
-        List<Badges> allBadgesByUser=userBadgeRepository.findByUser_UserId(customUserDetails.getUserId());
+        List<UserBadge> allBadgesByUser=userBadgeRepository.findByUser_UserId(customUserDetails.getUserId());
 
         return allBadgesByUser.stream()
                 .map(
                 b->allBadgesByUserResDto.builder()
-                        .badgeName(b.getBadgeName())
-                        .badgeImg(b.getBadgeImg())
+                        .badgeName(b.getBadge().getBadgeName())
+                        .badgeImg(b.getBadge().getBadgeImg())
                         .build()
         ).collect(Collectors.toList());
     }
