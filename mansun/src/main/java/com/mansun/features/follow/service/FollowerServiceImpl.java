73cc78bea7642a6d3e0dd67194325ff9e22244bf
이진형 <ddocdoli@naver.com>
2025-03-26 12:43:29 +0900

    feat : follower 인터페이스 추가

--- mansun/src/main/java/com/mansun/features/follow/service/FollowerServiceImpl.java
+++ mansun/src/main/java/com/mansun/features/follow/service/FollowerServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class FollowServiceImpl {
+public class FollowerServiceImpl implements FollowerService {
     private final FollowerRepositoy followerRepositoy;
     private final FollowingRepository followingRepository;
 
