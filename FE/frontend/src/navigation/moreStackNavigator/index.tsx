d7faa545ca2863b9932102088b38311209847f41
최이화 <ihwa220@naver.com>
2025-03-26 14:40:14 +0900

    더보기 헤더, 네비게이션 추가

--- FE/frontend/src/navigation/moreStackNavigator/index.tsx
+++ FE/frontend/src/navigation/moreStackNavigator/index.tsx
@@ -1,6 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { MoreStackParams } from "../../api/types/moreStackParams";
+import { CollectionListScreen } from "../../screens/collectionListScreen";
+import { FishingListScreen } from "../../screens/fishingListScreen";
 import { MoreScreen } from "../../screens/moreScreen";
+import { MyPostsScreen } from "../../screens/myPostsScreen";
 import { ProhibitedScreen } from "../../screens/prohibitedScreen";
 import { RuleScreen } from "../../screens/ruleScreen";
 import { TutorialScreen } from "../../screens/tutorialScreen";
@@ -11,9 +14,12 @@ export function MoreStackNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="More" component={MoreScreen} />
-      <Stack.Screen name="Tutorial" component={TutorialScreen} />
+      <Stack.Screen name="Myposts" component={MyPostsScreen} />
       <Stack.Screen name="Prohibited" component={ProhibitedScreen} />
       <Stack.Screen name="Rule" component={RuleScreen} />
+      <Stack.Screen name="Tutorial" component={TutorialScreen} />
+      <Stack.Screen name="FishingList" component={FishingListScreen} />
+      <Stack.Screen name="CollectionList" component={CollectionListScreen} />
     </Stack.Navigator>
   );
 }
