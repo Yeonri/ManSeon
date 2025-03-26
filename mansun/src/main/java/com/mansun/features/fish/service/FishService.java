f355f226f96976da05931857065714c103d195e5
이진형 <ddocdoli@naver.com>
2025-03-26 10:35:13 +0900

    feat : fish 기능 추가

---
+++ mansun/src/main/java/com/mansun/features/fish/service/FishService.java
@@ -0,0 +1,21 @@
+package com.mansun.features.fish.service;
+
+import com.mansun.common.auth.CustomUserDetails;
+import com.mansun.requestDto.fish.CreateFishReqDto;
+import com.mansun.requestDto.fish.FindFishReqDto;
+import com.mansun.responseDto.fish.FindFishListResDto;
+import com.mansun.responseDto.fish.FindFishResDto;
+
+import java.util.List;
+
+public interface FishService {
+    void createFish(CustomUserDetails customUserDetails, CreateFishReqDto req);
+
+    List<FindFishListResDto> findMyFishList(CustomUserDetails customUserDetails);
+
+    List<FindFishListResDto> findOthersFishList(CustomUserDetails customUserDetails, Long userId);
+
+    FindFishResDto findMyFish(CustomUserDetails customUserDetails,Long fishId);
+
+    FindFishResDto findOtherFish(CustomUserDetails customUserDetails,Long userId,Long fishId);
+}
