1ee0e106f14eeb92180fdaf316c4f25483563108
hyemin3699 <hyemin3699@naver.com>
2025-03-27 14:06:09 +0900

    feat: 지도 구현

--- FE/src/screens/mapScreen/index.tsx
+++ FE/src/screens/mapScreen/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import MapView from "react-native-maps";
 
 export function MapScreen() {
   return (
-    <View>
-      <Text>지도</Text>
+    <View className="flex-1">
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.5665,
+          longitude: 126.978,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
