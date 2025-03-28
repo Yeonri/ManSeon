6671c174bb2ac36d7789ed5682a7aab591d6f113
이진형 <ddocdoli@naver.com>
2025-03-28 11:17:59 +0900

    refactor : reqDto , CommentRepository 수정

--- BE/src/main/java/com/mansun/features/comment/repository/CommentRepository.java
+++ BE/src/main/java/com/mansun/features/comment/repository/CommentRepository.java
@@ -3,9 +3,10 @@ package com.mansun.features.comment.repository;
 import com.mansun.entity.board.Comment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 
 }
