a4a8fe03cd08155e30b039d3095cfce8873b9207
hyemin3699 <hyemin3699@naver.com>
2025-03-29 15:54:50 +0900

    refactor: 문의사항 버튼 기능 수정

--- FE/src/screens/suggestionsScreen/index.tsx
+++ FE/src/screens/suggestionsScreen/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import {
   ScrollView,
   Text,
@@ -7,8 +9,15 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HeaderBeforeTitle } from "../../components/common/headerBeforeTitle";
+import { SaveModal } from "../../components/more/saveModal";
 
 export function SuggestionsScreen() {
+  const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleSave = () => {
+    setModalVisible(true);
+  };
+
   return (
     <SafeAreaView className="flex-1">
       <HeaderBeforeTitle name="문의사항" />
@@ -29,13 +38,13 @@ export function SuggestionsScreen() {
 
           <View className="flex-1 flex-row justify-between px-5">
             <TouchableOpacity
-              onPress={() => {}}
+              onPress={() => navigation.goBack()}
               className="border border-blue-500 bg-white rounded-2xl w-40 py-3 items-center"
             >
               <Text className="text-blue-500 font-bold ">취소</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => {}}
+              onPress={handleSave}
               className="bg-blue-500 rounded-2xl py-3 w-40 items-center"
             >
               <Text className="text-white font-bold">저장</Text>
@@ -43,6 +52,10 @@ export function SuggestionsScreen() {
           </View>
         </View>
       </ScrollView>
+      <SaveModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
     </SafeAreaView>
   );
 }
