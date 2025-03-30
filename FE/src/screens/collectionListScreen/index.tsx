b7d5c0881e6b3a78621348dcc10cd5ef41d1f0f5
hyemin3699 <hyemin3699@naver.com>
2025-03-30 11:03:29 +0900

    feat: 도감 리스트 페이지 구현

--- FE/src/screens/collectionListScreen/index.tsx
+++ FE/src/screens/collectionListScreen/index.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CollectionCard from "../../components/collection/collectionCard";
+import { HeaderBeforeTitle } from "../../components/common/headerBeforeTitle";
+import processedFishData from "../../utils/processedFishData";
 
 export function CollectionListScreen() {
   return (
-    <View>
-      <Text>도감 리스트</Text>
-    </View>
+    <SafeAreaView edges={["top"]} className="flex-1 ">
+      <HeaderBeforeTitle name="낚시 도감" />
+
+      <FlatList
+        data={processedFishData}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="flex-1 px-5"
+        contentContainerClassName="gap-y-5 pb-2"
+        columnWrapperClassName="justify-between"
+        renderItem={({ item }) => (
+          <CollectionCard
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            isCollected={item.is_collected}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
