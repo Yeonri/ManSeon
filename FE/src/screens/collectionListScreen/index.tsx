9138b2c4e95efdefa545371f336990bce63e60cd
hyemin3699 <hyemin3699@naver.com>
2025-03-30 12:51:33 +0900

    feat: 드롭다운 버튼 구현

--- FE/src/screens/collectionListScreen/index.tsx
+++ FE/src/screens/collectionListScreen/index.tsx
@@ -1,16 +1,64 @@
-import { FlatList } from "react-native";
+import { useMemo, useState } from "react";
+import { FlatList, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CollectionCard from "../../components/collection/collectionCard";
+import Dropdown from "../../components/common/dropdown";
 import { HeaderBeforeTitle } from "../../components/common/headerBeforeTitle";
 import processedFishData from "../../utils/processedFishData";
 
+const sortOptions = ["가나다순", "잡은 물고기순", "최신순"];
+
 export function CollectionListScreen() {
+  const [selectedSort, setSelectedSort] = useState(sortOptions[0]);
+
+  const sortedData = useMemo(() => {
+    const copyData = [...processedFishData];
+
+    if (selectedSort === "가나다순") {
+      return copyData.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (selectedSort === "잡은 물고기순") {
+      return copyData
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .sort((a, b) => Number(b.is_collected) - Number(a.is_collected));
+    }
+
+    if (selectedSort === "최신순") {
+      return copyData.sort((a, b) => {
+        const aDate =
+          a.collection_info.length > 0
+            ? new Date(
+                a.collection_info[a.collection_info.length - 1].caught_at
+              ).getTime()
+            : 0;
+        const bDate =
+          b.collection_info.length > 0
+            ? new Date(
+                b.collection_info[b.collection_info.length - 1].caught_at
+              ).getTime()
+            : 0;
+        return bDate - aDate;
+      });
+    }
+
+    return copyData;
+  }, [selectedSort]);
+
   return (
     <SafeAreaView edges={["top"]} className="flex-1 ">
       <HeaderBeforeTitle name="낚시 도감" />
 
+      <View className="px-5 mt-4 mb-5 items-end">
+        <Dropdown
+          options={sortOptions}
+          selected={selectedSort}
+          onSelect={setSelectedSort}
+        />
+      </View>
+
       <FlatList
-        data={processedFishData}
+        data={sortedData}
         keyExtractor={(item) => item.id.toString()}
         numColumns={3}
         className="flex-1 px-5"
