a0be9b0d1af77e487f7d1fd37fd410d44870c553
이진형 <ddocdoli@naver.com>
2025-04-01 01:42:58 +0900

    refactor : 정보를 받는 kakaoInfo 정리

--- BE/src/main/java/com/mansun/common/auth/KakaoInfo.java
+++ BE/src/main/java/com/mansun/common/auth/KakaoInfo.java
@@ -2,16 +2,23 @@ package com.mansun.common.auth;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @RequiredArgsConstructor
+@ToString
 public class KakaoInfo {
-    private Long id;
+    private Long userId;
     private String nickname;
     private String email;
 
-    public KakaoInfo(Long id, String nickname, String email) {
-        this.id = id;
+    public KakaoInfo(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    public KakaoInfo(Long userId, String nickname, String email) {
+        this.userId = userId;
         this.nickname = nickname;
         this.email = email;
     }
