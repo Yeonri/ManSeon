3a72a6b3e42e71b4a3762af1a6d043cfa56073e2
이진형 <ddocdoli@naver.com>
2025-03-31 17:45:28 +0900

    fix : debug

--- BE/src/main/java/com/mansun/features/follow/service/FollowerServiceImpl.java
+++ BE/src/main/java/com/mansun/features/follow/service/FollowerServiceImpl.java
@@ -4,8 +4,10 @@ import com.mansun.features.follow.repository.FollowerRepositoy;
 import com.mansun.features.follow.repository.FollowingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class FollowerServiceImpl implements FollowerService {
     private final FollowerRepositoy followerRepositoy;
