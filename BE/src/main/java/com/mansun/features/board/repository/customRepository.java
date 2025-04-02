cfed74d1bd60d33da16bf9b11b1c5e22d9f8df20
이진형 <ddocdoli@naver.com>
2025-04-02 12:42:56 +0900

    refactor : customRepository 삭제와 파일 이름 변경

--- BE/src/main/java/com/mansun/features/board/repository/customRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mansun.features.board.repository;
-
-import com.mansun.entity.board.Board;
-
-import java.util.List;
-
-public interface customRepository {
-    List<Board> boardList();
-}
