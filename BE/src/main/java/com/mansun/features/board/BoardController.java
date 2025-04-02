6f73c9b858f866dbcd126a8e2d7fcca7ec8abd26
이진형 <ddocdoli@naver.com>
2025-04-02 15:31:29 +0900

    refactor : MessageDto로 파일 이름 변경 결과

--- BE/src/main/java/com/mansun/features/board/BoardController.java
+++ BE/src/main/java/com/mansun/features/board/BoardController.java
@@ -5,12 +5,11 @@ import com.mansun.features.board.service.BoardServiceImpl;
 import com.mansun.requestDto.board.CreateBoardReqDto;
 import com.mansun.requestDto.board.DeleteMyBoardReqDto;
 import com.mansun.requestDto.board.UpdateMyBoardReqDto;
-import com.mansun.responseDto.OnlyMessageResDto;
+import com.mansun.responseDto.MessageResDto;
 import com.mansun.responseDto.board.FindBoardResDto;
 import com.mansun.responseDto.board.FindMyBoardListResDto;
 import com.mansun.responseDto.board.allBoardListResDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -27,14 +26,13 @@ import java.util.List;
 public class BoardController {
     private final BoardServiceImpl boardservice;
 
-    // 내 게시글 작성
     @Operation(summary = "게시글 추가")
     @PostMapping
-    public ResponseEntity<OnlyMessageResDto> createBoard(
+    public ResponseEntity<MessageResDto> createBoard(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody CreateBoardReqDto req) {
         boardservice.createBoard(customUserDetails, req);
-        return ResponseEntity.ok(new OnlyMessageResDto("성공적으로 게시물이 생성되었습니다."));
+        return ResponseEntity.ok(new MessageResDto("성공적으로 게시물이 생성되었습니다."));
     }
 
     @Operation(summary = "전체 게시글 리스트")
@@ -43,14 +41,14 @@ public class BoardController {
     ) {
         return ResponseEntity.ok(boardservice.findAllBoardList());
     }
-    // 전체 게시글 상세 열람 (내 게시물 상세 열람 포함)
-    @Operation(summary = "전체 게시글 상세 열람")
+
+    @Operation(summary = "전체 게시글 상세 열람(내 게시물 상세 열람 포함)")
     @GetMapping("/all/detail")
     public ResponseEntity<FindBoardResDto> findBoard(
             @RequestParam(value = "board_id") long boardId) {
         return ResponseEntity.ok(boardservice.findBoard(boardId));
     }
-    // 내 게시글 리스트 열람
+
     @Operation(summary = "내 게시글 리스트 열람")
     @GetMapping("/myList")
     public ResponseEntity<List<FindMyBoardListResDto>> findMyBoardList(
@@ -59,30 +57,27 @@ public class BoardController {
         return ResponseEntity.ok(myBoardList);
     }
 
-    // 내 게시글 수정
     @Operation(summary = "내 게시글 수정")
     @PatchMapping
-    public ResponseEntity<OnlyMessageResDto> updateMyBoard(
+    public ResponseEntity<MessageResDto> updateMyBoard(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody UpdateMyBoardReqDto req) {
         boardservice.updateMyBoard(customUserDetails, req);
-        return ResponseEntity.ok(new OnlyMessageResDto("성공적으로 게시글이 수정되었습니다"));
+        return ResponseEntity.ok(new MessageResDto("성공적으로 게시글이 수정되었습니다"));
     }
 
-    // 내 게시글 삭제
     @Operation(summary = "내 게시글 삭제")
     @DeleteMapping
-    public ResponseEntity<OnlyMessageResDto> deleteMyBoard(
+    public ResponseEntity<MessageResDto> deleteMyBoard(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody DeleteMyBoardReqDto req) {
         boardservice.deleteMyBoard(customUserDetails, req);
-        return ResponseEntity.ok(new OnlyMessageResDto("성공적으로 게시글이 삭제되었습니다."));
+        return ResponseEntity.ok(new MessageResDto("성공적으로 게시글이 삭제되었습니다."));
     }
 
-    // 내 친구 게시글 리스트 열람
     @Operation(summary = "내 친구의 게시글 리스트 열람")
     @GetMapping
-    public ResponseEntity<OnlyMessageResDto> myFriendBoardList(
+    public ResponseEntity<MessageResDto> myFriendBoardList(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
     ) {
         return null;
--- BE/src/main/java/com/mansun/features/user/UserController.java
+++ BE/src/main/java/com/mansun/features/user/UserController.java
@@ -6,15 +6,20 @@ import com.mansun.features.user.service.UserServiceImpl;
 import com.mansun.requestDto.user.CreateUserReqDto;
 import com.mansun.requestDto.user.SetNicknameReqDto;
 import com.mansun.requestDto.user.UpdateUserReqDto;
-import com.mansun.responseDto.OnlyMessageResDto;
+import com.mansun.responseDto.MessageResDto;
+import com.mansun.responseDto.follow.GetMyFollowerResDto;
+import com.mansun.responseDto.follow.GetMyFollowingResDto;
 import com.mansun.responseDto.user.GetMyInfoResDto;
 import com.mansun.responseDto.user.GetTheOtherOneInfoResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,20 +31,20 @@ public class UserController {
 
     @Operation(summary = "회원가입")
     @PostMapping
-    public ResponseEntity<OnlyMessageResDto> createUser(@RequestBody CreateUserReqDto req) {
+    public ResponseEntity<MessageResDto> createUser(@RequestBody CreateUserReqDto req) {
         //아무것도 추가하지 않고 헤더만 추가해서 간다.
         //만약 오류가 난다면 service 안에서 예외처리 반환 예정
         userService.createUser(req);
-        return ResponseEntity.ok(new OnlyMessageResDto("회원가입 성공"));
+        return ResponseEntity.ok(new MessageResDto("회원가입 성공"));
     }
 
     @Operation(summary = "닉네임 설정")
     @PostMapping("/nickname/set")
-    public ResponseEntity<OnlyMessageResDto> setNickname(@RequestBody SetNicknameReqDto req) {
+    public ResponseEntity<MessageResDto> setNickname(@RequestBody SetNicknameReqDto req) {
         //아무것도 추가하지 않고 헤더만 추가해서 간다.
         //만약 오류가 난다면 service 안에서 예외처리 반환 예정
         userService.setNickname(req);
-        return ResponseEntity.ok(new OnlyMessageResDto("회원가입 성공"));
+        return ResponseEntity.ok(new MessageResDto("회원가입 성공"));
     }
 
     // 로그인 과정의 대략적인 설명 : 해당 요청 mapping은 이곳에 있지 않다.
@@ -49,28 +54,27 @@ public class UserController {
 
     @Operation(summary = "회원 정보 변경")
     @PatchMapping
-    public ResponseEntity<OnlyMessageResDto> updateUser(
+    public ResponseEntity<MessageResDto> updateUser(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
-            @RequestBody UpdateUserReqDto req) {
+            @Valid @RequestBody UpdateUserReqDto req) {
         Users user = userService.updateUser(customUserDetails, req);
-        return ResponseEntity.ok(new OnlyMessageResDto("회원 정보가 성공적으로 변경되었습니다."));
+        return ResponseEntity.ok(new MessageResDto("회원 정보가 성공적으로 변경되었습니다."));
     }
 
     @Operation(summary = "회원정보 삭제")
     @DeleteMapping
-    public ResponseEntity<OnlyMessageResDto> deleteUser(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-//        System.out.println(customUserDetails.getUserId());
+    public ResponseEntity<MessageResDto> deleteUser(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         userService.deleteUser(customUserDetails.getUserId());
-        return ResponseEntity.ok(new OnlyMessageResDto("회원 정보가 삭제되었습니다."));
+        return ResponseEntity.ok(new MessageResDto("회원 정보가 삭제되었습니다."));
     }
 
 
     //닉네임 중복 체크
     @Operation(summary = "닉네임 중복 확인")
     @GetMapping("/nickname/duplicate")
-    public ResponseEntity<OnlyMessageResDto> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
+    public ResponseEntity<MessageResDto> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
         userService.findByNickname(nickname);
-        return ResponseEntity.ok(new OnlyMessageResDto("사용가능한 닉네임입니다."));
+        return ResponseEntity.ok(new MessageResDto("사용가능한 닉네임입니다."));
     }
 
     @Operation(summary = "내 정보 가져오기")
@@ -91,19 +95,19 @@ public class UserController {
         return ResponseEntity.ok(userService.findTheOtherOneInfo(customUserDetails, req));
     }
 
-//    @Operation(summary = "팔로잉 리스트 가져오기")
-//    @GetMapping("/followings")
-//    public ResponseEntity<List<getMyFollowingResDto>> getMyFollowingList(
-//            @AuthenticationPrincipal CustomUserDetails customUserDetails
-//    ){
-//        return ResponseEntity.ok(userService.getMyFollowingFindById(customUserDetails));
-//    }
-//
-//    @Operation(summary="팔로워 리스트 가져오기")
-//    @GetMapping("/followers")
-//    public ResponseEntity<List<getMyFollowerResDto>> getMyFollowerList(
-//            @AuthenticationPrincipal CustomUserDetails customUserDetails
-//    ){
-//        return ResponseEntity.ok(userService.getMyFollowerFindById(customUserDetails));
-//    }
+    @Operation(summary = "팔로잉 리스트 가져오기")
+    @GetMapping("/followings")
+    public ResponseEntity<List<GetMyFollowingResDto>> getMyFollowingList(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        return ResponseEntity.ok(userService.getMyFollowingFindById(customUserDetails));
+    }
+
+    @Operation(summary="팔로워 리스트 가져오기")
+    @GetMapping("/followers")
+    public ResponseEntity<List<GetMyFollowerResDto>> getMyFollowerList(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        return ResponseEntity.ok(userService.getMyFollowerFindById(customUserDetails));
+    }
 }
