f16eb7f4e8b0b97f92cab6b79fdc13326ad9fba6
이진형 <ddocdoli@naver.com>
2025-04-03 22:34:21 +0900

    refactor : swagger schema 추가

--- BE/src/main/java/com/mansun/entity/fishingPoint/FishingPoint.java
+++ BE/src/main/java/com/mansun/entity/fishingPoint/FishingPoint.java
@@ -1,6 +1,7 @@
 package com.mansun.entity.fishingPoint;
 
 import com.mansun.entity.fishingPoint.dataSet.MarineZone;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,20 +25,28 @@ public class FishingPoint {
 
 //    Column
     @Column(name = "point_name")
+    @Schema(description = "낚시 포인트 이름")
     private String pointName;
+
     @Column(name = "lat")
+    @Schema(description = "위도")
     private float lat;
     @Column(name = "lng")
+    @Schema(description = "경도")
     private float lng;
     @Column(name = "primary_material")
+    @Schema(description = "저질(물 속의 땅 구성)")
     private String primaryMaterial;
     @Column(name = "weather_x")
+    @Schema
     private Integer weatherX;
     @Column(name = "weather_y")
     private Integer weatherY;
     @Column(name = "sunmoon_location")
+    @Schema(description = "일출 일몰 위치")
     private String sunmoonLocation;
     @Column(name = "nearest_obs_code")
+    @Schema(description = "가장 가까운 관측소")
     private String nearestObsCode;
     @Builder.Default
     private boolean deleted=false;
